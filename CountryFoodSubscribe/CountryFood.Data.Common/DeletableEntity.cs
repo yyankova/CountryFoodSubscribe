c23f7e5b242c4e6da425d89cc8fdac633df1dc55
yyankova <yana.o.yankova@gmail.com>
2014-11-10 12:29:28 +0200

    Add GenericRepository.cs and DeletableEntityRepository.cs

--- CountryFoodSubscribe/CountryFood.Data.Common/DeletableEntity.cs
+++ CountryFoodSubscribe/CountryFood.Data.Common/DeletableEntity.cs
@@ -2,11 +2,13 @@
 {
     using System;
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     public abstract class DeletableEntity : AuditInfo, IDeletableEntity
     {
         [Display(Name = "Deleted?")]
         [Editable(false)]
+        [Index]
         public bool IsDeleted { get; set; }
 
         [Display(Name = "Deletion date")]
---
+++ CountryFoodSubscribe/CountryFood.Data.Common/DeletableEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace CountryFood.Data.Common
+{
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class DeletableEntityRepository<T> : GenericRepository<T>, IDeletableEntityRepository<T>
+        where T : class, IDeletableEntity
+    {
+        public DeletableEntityRepository(DbContext context)
+            : base(context)
+        {
+        }
+
+        public override IQueryable<T> All()
+        {
+            return base.All().Where(x => !x.IsDeleted);
+        }
+
+        public IQueryable<T> AllWithDeleted()
+        {
+            return base.All();
+        }
+    }
+}
