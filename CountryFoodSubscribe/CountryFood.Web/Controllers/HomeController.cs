ca5a9773fcdc72d5da83c5fd9988c98511837548
yyankova <yana.o.yankova@gmail.com>
2014-11-12 10:59:04 +0200

    Simple display of producers and products on the Home page.

--- CountryFoodSubscribe/CountryFood.Web/Controllers/HomeController.cs
+++ CountryFoodSubscribe/CountryFood.Web/Controllers/HomeController.cs
@@ -1,18 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace CountryFood.Web.Controllers
+﻿namespace CountryFood.Web.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using CountryFood.Data;
+    using CountryFood.Web.Models;
+
     public class HomeController : Controller
     {
+        private IApplicationData data;
+
+        public HomeController() : this(new ApplicationData())
+        {
+        }
+
+        public HomeController(IApplicationData data)
+        {
+            this.data = data;
+        }
+
         public ActionResult Index()
         {
-            //return Redirect("http://google.bg");
-            string message = (string)ViewBag.Message;
             return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult GetProducts()
+        {
+            var products = this.data.Products
+                .All()
+                .Select(p => new ProductViewModel() { Name = p.Name })
+                .ToList();
+            return PartialView("~/Views/Shared/_ProductsPartial.cshtml", products);
+        }
+
+        [ChildActionOnly]
+        public ActionResult GetProducers()
+        {
+            var producers = this.data.Producers
+                .All()
+                .Select(p => new ProducerViewModel() { Name = p.Name })
+                .ToList();
+            return PartialView("~/Views/Shared/_ProducersPartial.cshtml", producers);
         }
     }
 }
