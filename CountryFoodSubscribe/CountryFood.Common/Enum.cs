44dbc6219a72f4a1ce20c8c9bf43cb4f1eb9c192
yyankova <yana.o.yankova@gmail.com>
2014-11-13 23:11:02 +0200

    Use Display custom attribute for enumeration Frequency

---
+++ CountryFoodSubscribe/CountryFood.Common/Enum.cs
@@ -0,0 +1,17 @@
+﻿namespace CountryFood.Common
+{
+    using System;
+    using System.ComponentModel;
+
+    public static class Enum<T>
+    {
+        public static string Description(T value)
+        {
+            DescriptionAttribute[] da = (DescriptionAttribute[])
+                (typeof(T)
+                .GetField(value.ToString())
+                .GetCustomAttributes(typeof(DescriptionAttribute), false));
+            return da.Length > 0 ? da[0].Description : value.ToString();
+        }
+    }
+}
--- CountryFoodSubscribe/CountryFood.Models/Frequency.cs
+++ CountryFoodSubscribe/CountryFood.Models/Frequency.cs
@@ -1,10 +1,15 @@
-﻿namespace CountryFood.Models
+﻿using System.ComponentModel;
+namespace CountryFood.Models
 {
     public enum Frequency
     {
+        [Description("Once a week")]
         Weekly,
+        [Description("Twice a month")]
         BiWeekly,
+        [Description("Once a month")]
         Monthly,
+        [Description("Once a year")]
         Yearly
     }
 }
